b36f74bbf39d79e3c23c49a884beab91fb06afd8
Vinay Kumar <kumarv5@student.douglascollege.ca>
2020-05-20 21:10:55 -0700

    Java project

--- src/myJavaProject/myAppTester.java
+++ src/myJavaProject/myAppTester.java
@@ -3,6 +3,8 @@
  */
 package myJavaProject;
 
+import java.util.*;
+
 /**
  * @author 12363
  *
@@ -14,7 +16,34 @@ public class myAppTester {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello Software Engineering World ");
+		
+	
+		
+		
+		  Scanner sc = new Scanner(System.in);
+		  
+	        System.out.print("Enter a number: ");
+	        int number = sc.nextInt();
+	 
+	        int sumOfDigits = findSumOfDigits(number);
+	        System.out.println("Sum of digits of " + number + " is " + sumOfDigits);
+	 
+	        
+	    }
+
+	 
+	    // Find sum of digits of a number
+	    public static int findSumOfDigits(int number) {
+	        int sum = 0;
+	        while (number > 0) {
+	            int digit = number % 10;
+	            number = number / 10;
+	            sum += digit;
+	        }
+	        return sum;
+	    }
 
 	}
 
-}
+
